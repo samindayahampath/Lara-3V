747340f020f3e564a0a417a12641992dd582d5b9
samindayahampath <samikolla2007@gmail.com>
2025-07-21 18:56:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+(const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "WE5DBJCK#7avE78LQpiCQg_uS0COi1efk6DObGUjblv39YtQyS10", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
